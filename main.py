67e43640aa4298be4f73caa2e78f286a30afd2f3
Ksushondrik <ksushondrik369@gmail.com>
2023-12-26 10:15:47 +0200

    додано приклад з f-рядками

--- main.py
+++ main.py
@@ -7,31 +7,45 @@ print("Hello Git")
 
 #Периметр квадрата
 
-a = float(input("Введіть сторону квадрата a: "))
-P = 4 * a
-print(f"Периметр кавдрата дорівнює {P}")
+#a = float(input("Введіть сторону квадрата a: "))
+#P = 4 * a
+#print(f"Периметр кавдрата дорівнює {P}")
 
 #Вартість їжі для кава-брейків
 
 #Встановлюємо ціни на продукти
-price_per_croissant = 1.04
-price_per_glass = 0.34
-price_per_coffee_pack = 4.42
+#price_per_croissant = 1.04
+#price_per_glass = 0.34
+#price_per_coffee_pack = 4.42
 
 #Кількість кожного продукту
-num_croissants = int(input("Введіть кількість круасанів: "))
-num_glasses = int(input("Введіть кількість склянок: "))
-num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+#num_croissants = int(input("Введіть кількість круасанів: "))
+#num_glasses = int(input("Введіть кількість склянок: "))
+#num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
 
 #Обчислення загальної вартості
-total_cost = num_croissants * price_per_croissant + \
-            num_glasses * price_per_glass + \
-            num_coffee_packs * price_per_coffee_pack
+#total_cost = num_croissants * price_per_croissant + \
+#            num_glasses * price_per_glass + \
+#            num_coffee_packs * price_per_coffee_pack
 
 #Визначаємо кількість повних доларів і центів
-total_dollars=int(total_cost)
-total_cents=int(total_cost*100)
+#total_dollars=int(total_cost)
+#total_cents=int(total_cost*100)
 
 #Вивід результату
-print(f"Загальна вартість у повних доларах: {total_dollars} доларів.")
-print(f"Загальна вартість у центах: {total_cents} центів.")
+#print(f"Загальна вартість у повних доларах: {total_dollars} доларів.")
+#print(f"Загальна вартість у центах: {total_cents} центів.")
+
+#f-рядки
+num_1=int(input("Введіть перше ціле число: "))
+num_2=int(input("Введіть друге ціле число: "))
+print(f"{num_1}+{num_2}={num_1+num_2}")
+print(f"{num_1}-{num_2}={num_1-num_2}")
+print(f"{num_1}*{num_2}={num_1*num_2}")
+print(f"{num_1}/{num_2}={num_1/num_2}")
+
+#f-рядки 2
+name=input("Введіть Ваше ім'я: ")
+last_name=input("Введіть Ваше прізвище: ")
+full_name=f"Ваше ім'я: {name} {last_name}"
+print(full_name)
